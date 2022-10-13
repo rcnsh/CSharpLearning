b67eadf3340c81d4233d5e69b914290e64e6effb
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2022-10-13 10:31:01 +0100

    finish section 1, challenge 14

--- Challenge011/Program.cs
+++ Challenge011/Program.cs
@@ -12,17 +12,47 @@ while (x == 0)
         Console.WriteLine("Enter a number of pounds to convert to euros." + Environment.NewLine);
 
         double pounds = Convert.ToDouble(Console.ReadLine());
-        double euros = pounds * 1.14;
+        double dobeuros = pounds * 1.14;
 
-        Console.WriteLine("£" + pounds + " is equal to € " + euros + Environment.NewLine);
+        Math.Round(dobeuros);
+        int euros = Convert.ToInt32(dobeuros);
+        
+        int fiftynotes = euros / 50;
+        int fiftyremainder = euros % 50;
+
+        int twentynotes = fiftyremainder / 20;
+        int twentyremainder = fiftyremainder % 20;
+
+        int tennernotes = twentyremainder / 10;
+        int tennerremainder = twentyremainder % 10;
+
+        int fivernotes = tennerremainder / 5;
+        int fiverremainder = tennerremainder % 5;
+        
+        Console.WriteLine("You would need " + fiftynotes + " fifty euro notes, " + twentynotes + " twenty euro notes, " + tennernotes + " ten euro notes, and " + fivernotes + " five euro notes to make this amount of money, with €" + fiverremainder + " left over.");
     }
     
     if (choice == 2)
     {
         Console.WriteLine("Enter a number of euros to convert to pounds." + Environment.NewLine);
         double euros = Convert.ToDouble(Console.ReadLine());
-        double pounds = euros / 1.14;
+        double dobpounds = euros / 1.14;
+        Math.Round(dobpounds);
+        int pounds = Convert.ToInt32(dobpounds);
+        
+        int fiftynotes = pounds / 50;
+        int fiftyremainder = pounds % 50;
 
+        int twentynotes = fiftyremainder / 20;
+        int twentyremainder = fiftyremainder % 20;
+
+        int tennernotes = twentyremainder / 10;
+        int tennerremainder = twentyremainder % 10;
+
+        int fivernotes = tennerremainder / 5;
+        int fiverremainder = tennerremainder % 5;
+        
+        Console.WriteLine("You would need " + fiftynotes + " fifty pounds notes, " + twentynotes + " twenty pounds notes, " + tennernotes + " ten pound notes, and " + fivernotes + " five pound notes to make this amount of money, with £" + fiverremainder + " left over.");
     }
 
     if (choice == 3)
